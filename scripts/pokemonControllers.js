7c1b4c7585c2e0f96c2264a1c236e96ac2abfcbc
rajasimhan123 <rajasimhanv@gmail.com>
2017-02-02 22:17:29 +0800

    commit

--- scripts/pokemonControllers.js
+++ scripts/pokemonControllers.js
@@ -9,13 +9,22 @@
 app.controller('mainController', function($scope, $http,pokeFactory,pokemonService) {
 
 	//Fetching the pokemon details from json using factory method
-	pokeFactory.fetchPokemonDetails().then(function(response){$scope.pokemons = response.data;});
+	pokeFactory.fetchPokemonDetails().then(function(response){
+		$scope.pokemons = response.data;
+		for(var i=0; i<$scope.pokemons.length;i++)
+		{
+				if($scope.pokemons[i].hasOwnProperty('flatName')){
+					$scope.pokemons[i].ename=$scope.pokemons[i].flatName;
+				}
+		}
+	});
 	//Fetching the pokemon type 
 	pokeFactory.fetchPokemonTypes().then(function(response){$scope.types = response.data;});
 	//Fetching the pokemon skills 
 	pokeFactory.fetchPokemonSkills().then(function(response){$scope.skills = response.data;});
 	
   
+
   // Retrieving the specific Pokemon details 
   $scope.open = function(pokemon){
       if ($scope.isOpen(pokemon)){
